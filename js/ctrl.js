5b367661c82842d6cfafdef6384de4af5eabe8f4
aldaiRubio <75966925+aldaiRubio@users.noreply.github.com>
2020-12-14 11:45:29 -0600

    Update ctrl.js

--- js/ctrl.js
+++ js/ctrl.js
@@ -1,12 +1,15 @@
 "use strict";
 const forma=document.getElementById("forma"),
      salida=document.getElementById("salida");
-forma.addEventListener("submit",mostrar,false);
-function mostrar(){
+forma.addEventListener("submit",mostrar=> try{
+    mostrar.preventDefault();
     const nombre=forma["nombre"].getValue,
           grupo=forma["grupo"].getValue,
           materia=forma["materia"].getValue,
           fecha=forma["fecha"].getValue;
     salida.value=nombre + "\n" + grupo + "\n" + materia + "\n" + fecha;
-}
+}cathc(error){
+     console.log(error);
+     salida.value=error.message;
+},false);
 
