3afc38da655192e85626f467ccf6a701a044c2fe
aldaiRubio <94ld4s@gmail.com>
2021-01-23 16:38:47 -0600

    jalo

--- js/init.js
+++ js/init.js
@@ -1,9 +1,10 @@
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyB-1o8X5PhcHC9H-aLKyj5FKtH46NMYEkM",
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase. */
+// @ts-ignore
+firebase.initializeApp({apiKey: "AIzaSyB-1o8X5PhcHC9H-aLKyj5FKtH46NMYEkM",
     authDomain: "aldairubio-a6985.firebaseapp.com",
     projectId: "aldairubio-a6985",
     storageBucket: "aldairubio-a6985.appspot.com",
     messagingSenderId: "122143194738",
     appId: "1:122143194738:web:ca4d0a210df881d17d4ceb"
-  };
+  });
